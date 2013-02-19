f9f294dca4081b100134083bcafdc69e0379da5e
bmtbandit <bmtbandit@bmtbandit-HP-EliteBook-2760p>
2013-02-19 17:42:49 -0500

    update psedu physics

--- gameengine/includes/physicsinfo.h
+++ gameengine/includes/physicsinfo.h
@@ -2,6 +2,23 @@
 #define PHYSICS_INFO_H
 
 //Shape, position, rotation, mass, moment of inertia, linear and angular velocities
-typedef struct physics_info_t {
-	int temp; //insert info here
+// Maintains physics-relevant information: shapes, mass, velocities, accelerations, etc.
+/*Shape
+Position (of the CM)
+Mass
+Linear velocity
+Linear acceleration
+Angular velocity
+Angular acceleration*/
+typedef struct physics_info_t { //all units MKS: meters, kilograms, seconds
+	float xPos;
+	float yPos;
+	float zPos;
+	float mass;
+	float linearVelovity;
+	float angularVelocity;
+	float linearAcceleration;
+	float angularAcceleration;
+	//point centerOfMass
+
 } physicsInfo;
--- gameengine/src/physicsengine.cpp
+++ gameengine/src/physicsengine.cpp
@@ -57,6 +57,32 @@ PhysicsEngine::freeCollisions(collisionDetection* collisions)
 	}
 }
 
+
+void calculateCenterOfMass()
+{
+	//totalcm = sum(mass(part) * center(part)/sum(mass(total)))
+
+}
+
+void calculateSpeed()
+{
+	//speed = initspeed + acc*t
+	// using verlet integration
+	//v(t2) = pos(t2) - pos(t1)/change in time
+}
+
+void calculateAcceleration()
+{
+	//acc= force/mass
+}
+
+void calculatePosition()
+{
+	// pos = initpos + v*t basic formula
+	// using verlet integration
+	//pos(2) = 2*pos - pos(curtime - change in time) + (Force(t1)/mass)*(change in time)^2
+}
+// combined formula pos = initpos + intvel*t + 1/2 acc *t^2
 /*•  You will need:
 •  Constructor: initializes an empty “physics world”
 •  Set gravity
